4d7bb45a47afe9f876d8f2d02a17e5d7b7dcc389
barlach2 <bruna.barlach@gmail.com>
2021-04-19 15:50:31 -0300

    exercícios em desenvolvimento

---
+++ semana3/variaveis/js-vanilla-template/src/index.js
@@ -0,0 +1,42 @@
+//Exercício 1
+//10
+//10, 5
+
+//Exercício 2
+//10, 10, 10
+
+let nome
+let idade
+console.log(typeof nome)
+console.log(typeof idade)
+//está como undefined porque não defini nenhum valor às vaviáveis
+let nomeOutro = prompt("Qual seu nome?")
+let idadeOutra = prompt("Qual sua idade?")
+console.log(typeof nomeOutro)
+console.log(typeof idadeOutra)
+//agora a saída é string porque foram atribuídos valores pelo prompt
+console.log(nomeOutro, idadeOutra)
+
+let cor = prompt("Qual sua cor preferida?")
+let comida = prompt("Qual sua comida referida")
+let livro = prompt("Qual seu livro preferido?")
+let filme = prompt("Qual seu filme preferido?")
+let serie = prompt("Qual sua série preferida?")
+console.log("Qual sua cor preferida?")
+console.log(cor)
+console.log("Qual sua comida referida")
+console.log(comida)
+console.log("Qual seu livro preferido?")
+console.log(livro)
+console.log("Qual seu filme preferido?")
+console.log(filme)
+console.log("Qual sua série preferida?")
+console.log(serie)
+
+let comidas = ["batata frita", "batata assada", "purê de batatata", "maionese de batata", "batata cozida frita"]
+console.log(comidas)
+console.log("Essas são as minhas comidas preferidas: ",)
+console.log(comidas [0])
+console.log(comidas [1])
+console.log(comidas [2])
+console.log(comidas [3])
