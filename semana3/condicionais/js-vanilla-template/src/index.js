b548511c2627ed8f426388e256ce52cebf240d0a
barlach2 <bruna.barlach@gmail.com>
2021-04-22 15:06:17 -0300

    Exercício em desenvolvimento

---
+++ semana3/condicionais/js-vanilla-template/src/index.js
@@ -0,0 +1,31 @@
+//Exercício 1
+//O programa testa se um número é par. Se for par, ele passa no teste, se não for, ele não passa.
+
+//Exercício 2
+//a. O código mostra o preço das frutas.
+//b. 2.25
+//c. 5.
+
+//Exercício 3
+//a. A primeira linha de código recebe o número digitado pelo usuário
+//b. Se digitar 10, Esse número passou no teste. Se digitar -10, não há mensagem.
+//c. Haverá erro porque a variável mensagem não é global.
+
+//Exercício 4
+let idade = Number(prompt("Digite uma idade"))
+if (idade > 18) {
+    console.log("Você pode dirigir!")
+}
+else {
+    console.log("Você não pode dirigir.")
+}
+
+//Exercício 5
+let periodo = prompt("Digite M, V, ou N")
+if (periodo = M) {
+    console.log("Bom dia!")
+} if (periodo = V) {
+    console.log("Boa Tarde!")
+} else {
+    console.log("Boa noite!")    
+}
