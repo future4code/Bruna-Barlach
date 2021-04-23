a7f310d59919f9023c6e073f2709850a43664011
Bruna Barlach <bruna.barlach@gmail.com>
2021-04-23 15:27:15 -0300

    Exercício em desenvolvimento

---
+++ semana3/loops/js-vanilla-template/src/index.js
@@ -0,0 +1,24 @@
+//Exercício 1
+//Enquanto i for menor que 5 o programa irá somar 1.
+
+//Exercício 2
+//a. Serão impressos os números da array maiores de 18
+//b.
+
+//Exercício 3
+//a.
+const numeros = [80, 30, 130, 40, 60, 21, 70, 120, 90, 103, 110, 55]
+for(let numero of numeros) {
+    console.log(numero)
+}
+//b.
+for(let numero of numeros) {
+    console.log(numero / 10)
+}
+//c.
+const resultado = []
+for(let numero of numeros) {
+    if numero % 2 > 0 {
+
+    }
+}
