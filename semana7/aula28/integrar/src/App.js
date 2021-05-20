16e213fc6f1b57f4a4a6ef44db7e01aea79c7705
barlach2 <bruna.barlach@gmail.com>
2021-05-20 16:22:17 -0300

    exercício realizado

--- semana7/aula28/integrar/src/App.js
+++ semana7/aula28/integrar/src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import axios from "axios"
+import styled from 'styled-components'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const CardAtividade = styled.div `
+border: 1px dotted gray;
+width: 300px;
+margin: 10px;
+padding: 10px;
+`
+
+export default class App extends React.Component {
+  state = {
+    activity: {}
+  }
+
+pegaAtividade = () => {
+  axios.get("http://www.boredapi.com/api/activity")
+  .then((res) => {
+    this.setState({activity: res.data})
+  })
 }
 
-export default App;
+
+render() {
+
+  const {activity, type, participants, link} = this.state.activity
+
+  return(
+    <div>
+        <h1>Bora fazer alguma coisa?</h1>
+        <button onClick={this.pegaAtividade}>Dá uma ideia</button>
+        <hr />
+      <CardAtividade>
+        <h2>Atividade</h2>
+        <p>Nome: {activity} </p>
+        <p>Tipo: {type}</p>
+        <p>Participantes: {participants}</p>
+        <p>Link: {link}</p>
+        </CardAtividade>
+    </div>
+
+  )
+}
+}
