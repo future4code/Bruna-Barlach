ffc798cbb5d838f34e4f5caeac52480164cf83d4
barlach2 <bruna.barlach@gmail.com>
2021-05-19 23:01:16 -0300

    exercício finalizado

--- semana7/aula26/cadastro-usuarios/src/App.js
+++ semana7/aula26/cadastro-usuarios/src/App.js
@@ -1,14 +1,35 @@
 import React from "react"
+import TelaListaUsuarios from "./components/TelaListaUsuarios"
+import TelaCadastro from "./components/TelaCadastro"
 
 export default class App extends React.Component {
   state = {
     telaAtual: "cadastro"
   }
 
+  escolheTela = () => {
+    switch (this.state.telaAtual){
+      case "cadastro":
+        return <TelaCadastro irParaLista={this.irParaLista} />
+      case "lista":
+        return <TelaListaUsuarios irParaCadastro={this.irParaCadastro} />
+      default:
+        return <div>Página não encontrada!</div>
+    }
+  }
+
+  irParaCadastro = () => {
+    this.setState({telaAtual: "cadastro"})
+  }
+
+  irParaLista = () => {
+    this.setState({telaAtual: "lista"})
+  }
+
     render(){
     return (
       <div>
-        Oi
+        {this.escolheTela()}
       </div>
     )
   }
