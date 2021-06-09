1616f6ed0982da62a68530b45bae66aef3647bd4
barlach2 <bruna.barlach@gmail.com>
2021-06-09 16:20:52 -0300

    criando login

--- semana10/labex/src/pages/LoginPage.js
+++ semana10/labex/src/pages/LoginPage.js
@@ -1,12 +1,28 @@
 import React from "react"
+import useInput from "../hooks/useinput"
+import {BASE_URL} from "../constants/urls"
+import axios from "axios";
 
 const LoginPage = () => {
 
+    const [email, handleEmail] = useInput("")
+    const [password, handlePassword] = useInput("")
+
+    const onClickLogin = () => {
+        const body = {email, password}
+        axios.post(`${BASE_URL}/login`, body)
+    }
+
     return (
-            <p>Página de Login</p>
-        );
-       
+        <div>
+            <h3>Página de Login</h3>
+            <input value={email} onChange={handleEmail} placeholder="E-mail" />
+            <input value={password} onChange={handlePassword} placeholder="Senha" />
+            <button onClick={onClickLogin}>Login</button>
+        </div>
+    );
+
 
 };
 export default LoginPage;
- 
+
