d3815ea72946b7f76dda4d44407bfc5b0892fd0d
barlach2 <bruna.barlach@gmail.com>
2021-06-24 13:37:34 -0300

    atualização

--- semana12/labeddit/src/components/Header/styled.js
+++ semana12/labeddit/src/components/Header/styled.js
@@ -1,10 +1,11 @@
 import styled from "styled-components";
  
-export const divHeader = styled.div `
+export const divHeader = styled.div`
 background-color: purple;
 border-bottom: 2px solid gray;
 display: flex;
 flex-direction: row;
 justify-content: space-between;
+
 ` ;
 
